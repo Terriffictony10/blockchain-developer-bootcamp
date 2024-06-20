5c729ead534db4054c8a7a931d6ce05654990903
Tony Yonan <superzman15@gmail.com>
2024-06-20 00:09:22 -0700

    3.2 finished writting testing for smart contracts

--- test/Token.js
+++ test/Token.js
@@ -10,24 +10,31 @@ describe('Token', () => {
 
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
-		token = await Token.deploy()
+		token = await Token.deploy("TonyToken", "TTK", "1000000")
 	})
 
+	describe("Deployment", () => {
+		const name = "TonyToken"
+		const symbol = "TTK"
+		const decimals = "18"
+		const totalSupply = "1000000"
+
 	it('Has the correct name', async () => {
-		expect(await token.name()).to.equal("TonyToken")
+	expect(await token.name()).to.equal(name)
 
 	})
 	it('Has the correct symbol', async () => {
-		expect(await token.symbol()).to.equal("TTK")
+		expect(await token.symbol()).to.equal(symbol)
 
 	})
 	it('Has the correct decimals', async () => {
-		expect(await token.decimals()).to.equal("18")
+		expect(await token.decimals()).to.equal(decimals)
 
 	})
 	it('Has the correct totalSupply', async () => {
-		expect(await token.totalSupply()).to.equal(tokens('1000000'))
+		expect(await token.totalSupply()).to.equal(tokens(totalSupply))
 
+	})
 	})
 })
 
